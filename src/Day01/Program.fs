5df319cccb136fe9adddcedfdff3bfd23e65e0fb
Anton Herzog <info@antonherzog.com>
2024-12-17 16:48:57 +0100

    Day 01 Part 1

--- src/Day01/Program.fs
+++ src/Day01/Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Day01
+open System
+
+let ints = 
+    Text.Split('\n')
+    |> Array.map (fun s -> s.Split(' '))
+    |> Array.map (Array.filter (fun s -> s <> ""))
+    |> Array.map (fun a -> (int a.[0], int a.[1]))
+
+let firsts = ints |> Array.map fst |> Array.sort
+let seconds = ints |> Array.map snd |> Array.sort
+
+let distance (a, b) =
+    a - b |> Int32.Abs
+
+let sum = 
+    Array.zip firsts seconds
+    |> Array.sumBy distance
