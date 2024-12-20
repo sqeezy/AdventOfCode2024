77e0ac0b08fb97e5f0c4dd10a6b2ef927a246a58
Anton Herzog <info@antonherzog.com>
2024-12-20 13:46:02 +0100

    Day 05 Part One

--- src/Day05/PartOne.fsx
+++ src/Day05/PartOne.fsx
@@ -1,11 +1,52 @@
-open System.Text.RegularExpressions
-
 let splitAt (c: char) (string: string) = string.Split(c)
 
 let inspect (x: 'a) =
     printfn "%A" x
     x
 
-let rulesText = System.IO.File.ReadAllText $"""{__SOURCE_DIRECTORY__}/rules.txt"""
-let updatesText = System.IO.File.ReadAllText $"""{__SOURCE_DIRECTORY__}/updates.txt"""
+let rulesText = System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}/rules.txt"""
+let updatesText =
+    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}/updates.txt"""
 
+type MustBeBefore = { First: int; Second: int }
+
+type Rule = MustBeBefore of MustBeBefore
+let parseRule (line: string) =
+    line
+    |> splitAt '|'
+    |> Array.map (fun x -> x.Trim())
+    |> (fun x ->
+        { First = x.[0] |> int
+          Second = x.[1] |> int })
+
+type Update = int list
+let parseUpdate (line: string) : Update =
+    line
+    |> splitAt ','
+    |> Array.map (fun x -> x.Trim())
+    |> Array.map int
+    |> List.ofArray
+
+let satisfiesMustBeBefore (rule: MustBeBefore) (update: Update) =
+    let firstIndex = update |> List.tryFindIndex (fun x -> x = rule.First)
+    let secondIndex = update |> List.tryFindIndex (fun x -> x = rule.Second)
+
+    match firstIndex, secondIndex with
+    | Some firstIndex, Some secondIndex -> firstIndex < secondIndex
+    | _ -> true
+
+let validateUpdate (rules: MustBeBefore array) (update: Update) =
+    rules
+    |> Array.forall (fun rule -> satisfiesMustBeBefore rule update)
+
+let getMiddleNumberOfUpdate (update: Update) =
+    update.[update.Length/2]
+
+let rules = rulesText |> Array.map parseRule
+let updates = updatesText |> Array.map parseUpdate
+
+let partOne = 
+    updates 
+    |> Array.filter (validateUpdate rules)
+    |> Array.map getMiddleNumberOfUpdate
+    |> Array.sum
